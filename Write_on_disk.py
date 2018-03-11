19e0dbdc7f22f442da845c45aad4da3533d995ff
luciancristian <cristianlucian84@gmail.com>
2018-03-11 17:07:43 +0200

    Copies itself repeatedly

---
+++ Write_on_disk.py
@@ -0,0 +1,13 @@
+import os
+from shutil import copyfile
+n=0
+if (os.name == 'nt'):
+    
+    while (n<10):
+        copyfile(os.getcwd()+'\Write_on_disk.py', os.getcwd()+'\Write_on_disk_copy_{}.py'.format(n))
+        n+=1
+    
+    
+    
+print (os.getcwd())
+print (os.name)
